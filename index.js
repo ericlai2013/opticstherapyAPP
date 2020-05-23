52945f8e4442acfdc6452c89b87ee11ce4621bcd
Eric Lai <laisiutak@gmail.com>
2020-05-24 00:15:00 +0800

    add validation module Joi

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
+// npm i @hapi/joi
+// document: https://hapi.dev/module/joi/
+
+const Joi = require('joi');
+
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
 
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
 //
+// app.get('/api/courses/:id', (req, res) => {
+//     res.send(req.params.id);
+// });
+
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+   // res.send(req.query);
+   const course = courses.find( c => c.id === parseInt(req.params.id) );
+
+   if(!course) res.status(404).send('The course with the given ID was not found!');
+
+   res.send(course);
 });
 
 // app.get('/api/posts/:year/:month', (req, res) => {
@@ -20,6 +42,31 @@ app.get('/api/courses/:id', (req, res) => {
 
 app.get('/api/posts/:year/:month', (req, res) => {
     res.send(req.query);
+});
+
+app.post('/api/courses', (req, res) => {
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+    console.log(result);
+
+
+    if(!req.body.name || req.body.name.length < 3) {
+        // 400 bad request
+        res.status(400).send('Name is required and should be minimum 3 characters.');
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+
+    courses.push(course);
+    res.send(course);
 });
 
 // PORT
